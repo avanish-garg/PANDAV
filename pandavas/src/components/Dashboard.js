6dc1678fa1a65c1eb1ab1dc9c375d5d03833c961
Avanish Garg <avanishgarg6734@gmail.com>
2024-06-13 23:31:48 +0530

    feat: Add bootstrap CSS to project & Created Login.js,Signup.js&Dashboard.js

---
+++ pandavas/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+
+function Dashboard() {
+  return (
+    <Container className="mt-5">
+      <h2>Dashboard</h2>
+      {/* Dashboard content goes here */}
+    </Container>
+  );
+}
+
+export default Dashboard;
