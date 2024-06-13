86c38fcbfbfb1f8b68e27a0a24884c62ff880000
Avanish Garg <avanishgarg6734@gmail.com>
2024-06-14 01:39:53 +0530

    Backend folder created with env file yet to be configured

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import process from 'process';
+
+dotenv.config();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // for parsing application/json
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Could not connect to MongoDB', err));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
