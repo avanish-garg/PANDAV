ac1c0f3c3acc5465ed61ac3827667406e307b881
Avanish Garg <avanishgarg6734@gmail.com>
2024-06-15 01:43:37 +0530

    feat: Add Socket.IO integration for real-time messaging event handling client

--- pandavas/src/App.js
+++ pandavas/src/App.js
@@ -1,11 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import io from 'socket.io-client';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Dashboard from './components/Dashboard';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Assuming your Socket.IO server is running on this URL
+const SOCKET_URL = 'http://localhost:4000';
+
 function App() {
+  useEffect(() => {
+    // Connect to Socket.IO server
+    const socket = io(SOCKET_URL);
+
+    // Common event handlers
+    socket.on('connect', () => {
+      console.log('Connected to Socket.IO server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from Socket.IO server');
+    });
+
+    // Example custom event handler
+    socket.on('message', (message) => {
+      console.log('New message:', message);
+    });
+
+    // Cleanup on component unmount
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
     <Router>
       <Routes>
