7e5f0869de4001b31f728f3f27f1d60a34377414
Avanish Garg <avanishgarg6734@gmail.com>
2024-06-16 13:55:20 +0530

    hi

--- pandavas/src/App.js
+++ pandavas/src/App.js
@@ -1,23 +1,21 @@
+// App.js
+
 import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import io from 'socket.io-client';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Dashboard from './components/Dashboard'; // Import the Dashboard component
+import Dashboard from './components/Dashboard';
 import QuillEditor from './components/QuillEditor';
 import DocumentEditor from './components/DocumentEditor';
 import ProfilePage from './components/ProfilePage';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-// Assuming your Socket.IO server is running on this URL
 const SOCKET_URL = 'http://localhost:5000';
 
 function App() {
   useEffect(() => {
-    // Connect to Socket.IO server
     const socket = io(SOCKET_URL);
-
-    // Common event handlers
     socket.on('connect', () => {
       console.log('Connected to Socket.IO server');
     });
@@ -26,12 +24,6 @@ function App() {
       console.log('Disconnected from Socket.IO server');
     });
 
-    // Example custom event handler
-    socket.on('message', (message) => {
-      console.log('New message:', message);
-    });
-
-    // Cleanup on component unmount
     return () => {
       socket.disconnect();
     };
@@ -42,7 +34,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} /> {/* Route for the Dashboard component */}
+        <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/editor/quill" element={<QuillEditor />} />
         <Route path="/edit-document/:documentId" element={<DocumentEditor />} />
         <Route path="/profile" element={<ProfilePage />} />
